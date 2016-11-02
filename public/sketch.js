f72eb09eb7e9df992e755a9179b7c9afe13aded0
extra-mayo <zjz204@nyu.edu>
2016-11-02 11:48:22 -0400

    Test

--- public/sketch.js
+++ public/sketch.js
@@ -19,7 +19,7 @@ function preload() {
 
 function setup() {
     createCanvas(1000, 720);
-    socket = io.connect("https://discount-agario.herokuapp.com/", { rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling']});
+    socket = io.connect("https://discount-agario.herokuapp.com/", {transports:['websocket']});
     imageMode(CENTER);
     image(menuScreen, width / 2, height / 2, 1280, 720);
 }
